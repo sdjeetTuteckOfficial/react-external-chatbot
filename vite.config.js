292b50f0d52319893b629767c90b10173854f3cc
sdjeetTuteckOfficial <shubhajeet.das@tuteck.com>
2025-03-17 16:17:11 +0530

    vite configuration updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensures the build goes to `dist`
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js', // Ensures the main JS file is named `index.js`
+      },
+    },
+  },
+});
